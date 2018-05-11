e14ff2f2b7e34a3670d3cccff831f2321ae48e9b
GuieMigi <florinchiriac2004@yahoo.com>
2018-05-11 20:32:54 +0300

    Added ArticleActivity logic

--- app/src/main/java/com/example/android/newsfeed/ArticleActivity.java
+++ app/src/main/java/com/example/android/newsfeed/ArticleActivity.java
@@ -1,16 +1,98 @@
 package com.example.android.newsfeed;
 
 import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ArticleActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<Article>> {
 
+    // Tag for the log messages.
+    private static final String LOG_TAG = ArticleActivity.class.getName();
+    //JSON response link for a Guardian api query.
+    private static final String GUARDIAN_JSON_QUERY = "https://content.guardianapis.com/search?&format=json&from-date=2018-05-01&use-date=first-publication&show-references=contributor&total=50&api-key=5dfacf98-e445-40d8-b686-1c3ecb66e8f4";
+    // TextView that is displayed when the list is empty.
+    TextView emptyStateTextView;
+    // Variable used to check the network state.
+    NetworkInfo networkInfo;
+    // Adapter for the list of articles.
+    private ArticleAdapter articleAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find a reference to the ListView in the layout.
+        ListView articleListView = findViewById(R.id.main_activity_listView);
+        // Find a reference to the empty state TextView.
+        emptyStateTextView = findViewById(R.id.empty_state_textView);
+        // Create a new adapter that takes an empty list of articles as input.
+        articleAdapter = new ArticleAdapter(this, new ArrayList<Article>());
+        // Set the adapter on the ListView.
+        articleListView.setAdapter(articleAdapter);
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnectedOrConnecting()) {
+            // Initialise the LoaderManager.
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.initLoader(1, null, ArticleActivity.this);
+        } else {
+            // If there is no network connection hide the ProgressBar and set the "No internet connection available." on the empty state TextView.
+            ProgressBar progressBar = findViewById(R.id.main_activity_progressBar);
+            emptyStateTextView.setText("No internet connection available");
+        }
+
+        articleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Find the current article that was clicked.
+                Article currentArticle = articleAdapter.getItem(position);
+                // Create a new intent to view the earthquake URI.
+                Intent openArticleWebsite = new Intent(Intent.ACTION_VIEW);
+                openArticleWebsite.setData(Uri.parse(currentArticle.getArticleWebsite()));
+                startActivity(openArticleWebsite);
+            }
+        });
+    }
+
+    @Override
+    public Loader<ArrayList<Article>> onCreateLoader(int i, Bundle bundle) {
+        return new ArticleLoader(ArticleActivity.this, GUARDIAN_JSON_QUERY);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ArrayList<Article>> loader, ArrayList<Article> articles) {
+        // Hide the ProgressBar when the background thread has finished loading the data.
+        ProgressBar progressBar = findViewById(R.id.main_activity_progressBar);
+        progressBar.setVisibility(View.GONE);
+
+        // Set empty state text to display "No earthquakes found."
+        emptyStateTextView.setText("No articles found.");
+
+        // If there is a valid list of articles, then add them to the adapter's data set.
+        if (articles != null && !articles.isEmpty()) {
+            articleAdapter.addAll(articles);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<ArrayList<Article>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        articleAdapter.clear();
+
     }
 }
--- app/src/main/java/com/example/android/newsfeed/ArticleAdapter.java
+++ app/src/main/java/com/example/android/newsfeed/ArticleAdapter.java
@@ -1,9 +1,13 @@
 package com.example.android.newsfeed;
 
+import android.app.Activity;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
 public class ArticleAdapter extends ArrayAdapter<Article> {
     // TODO: add the ArticleAdapter logic.
+    public ArticleAdapter(Activity context, ArrayList<Article> articles) {
+        super(context, 0, articles);
+    }
 }
